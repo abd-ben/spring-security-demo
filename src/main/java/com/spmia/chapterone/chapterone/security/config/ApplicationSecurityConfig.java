e42010b552764f638902370f87e453b3fc7941e3
Abderrahmane BENKHOUYA <abd.ben.ka@gmail.com>
2020-11-06 18:09:57 +0100

    AB | add thymeleaf dependency & override login page

--- src/main/java/com/spmia/chapterone/chapterone/security/config/ApplicationSecurityConfig.java
+++ src/main/java/com/spmia/chapterone/chapterone/security/config/ApplicationSecurityConfig.java
@@ -35,7 +35,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
     }
 
     @Override
